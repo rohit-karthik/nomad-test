6e16d301c67fcad3a650af5d806008c79f26e42b
Rohit Karthik <63020497+rohit-karthik@users.noreply.github.com>
2022-06-25 10:38:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def mainPage():
-    return "<h1>Flask with Docker and Nomad update!!!!</h1>"
+    return "<h1>Flask with Docker and Nomad this is cool!!!!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
